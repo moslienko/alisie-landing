e68cf482a476c98823e81b85203d4475ccb7537f
moslienko <8676976+moslienko@users.noreply.github.com>
2026-06-25 21:35:23 +0700

    Add app page

---
+++ src/components/index.ts
@@ -0,0 +1,25 @@
+import Navbar from "./navbar/Navbar";
+import Header from "./Header/Header"
+import HowItWorks from "./HowItWorks/HowItWorks"
+import Features from "./Features/Features"
+import Pricing from "./Pricing/Pricing"
+import ProMax from "./Pricing/ProMax"
+import Privacy from "./Privacy/Privacy"
+import FAQ from "./FAQ/FAQ"
+import FinalCTA from "./FinalCTA/FinalCTA"
+import Footer from "./Footer/Footer"
+import ThemeToggle from "./ThemeToggle/ThemeToggle"
+
+export {
+    Navbar,
+    Header,
+    HowItWorks,
+    Features,
+    Pricing,
+    ProMax,
+    Privacy,
+    FAQ,
+    FinalCTA,
+    Footer,
+    ThemeToggle
+}
