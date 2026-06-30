94be44c169bd74f08b199e3c2be7de2a4d95f2d7
moslienko <8676976+moslienko@users.noreply.github.com>
2026-06-30 10:59:54 +0700

    add 404 page

--- src/data/changelog.en.ts
+++ src/data/changelog.en.ts
@@ -2,7 +2,7 @@ import type { ChangelogEntry } from "./changelog.types";
 
 export const changelogEn: ChangelogEntry[] = [
   {
-    version: "2.0.0",
+    version: "2.0.1",
     date: "2026-06-30",
     footer: "We're now Alisie! Same app, new name.",
     notes: [
